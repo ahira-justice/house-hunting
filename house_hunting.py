4548d9b10ea5f1596269e98a97981302db2aa798
Ahira <justiceahira@gmail.com>
2020-04-13 05:37:53 +0100

    Add documentation

--- house_hunting.py
+++ house_hunting.py
@@ -1,3 +1,40 @@
+"""
+Read the problem statement carefully.
+
+* Hint no.1 advises on how to get user input, get_inputs() implements this
+
+    Observe the float() conversions.
+
+* Hint no.2 advises on how to initialize variables
+    
+    Observe the careful names chosen for initialization of the variables.
+    Also observe, that not all initialization is to the input values.
+
+    current_savings and months are 0, as they should be.
+    r is 0.04 or 4% as defined in the problem.
+
+    monthly_portion_saved and portion_down_payment are derived values.
+    These are the values that go into the core calculation area.
+
+* Core calculation area
+
+    The condition is satisfied when our MIT graduate has enough saved for a down payment.
+
+    So, we have:
+    while current_savings < portion_down_payment:
+
+    While we increment our months, we also increment on our current_savings.
+
+    Incrementing current_savings by the right ammount, we have:
+    current_savings = current_savings + monthly_portion_saved + monthly_current_savings_return
+
+    monthly_current_savings_return is obtained from calculate_monthly_current_savings_return().
+
+This implentation passes the two test cases provided in the problem statement.
+
+"""
+
+
 def main():
     months = 0
     annual_salary, portion_saved, total_cost = get_inputs()
@@ -17,6 +54,8 @@ def main():
 
 
 def get_inputs():
+    """Get input from user and return float equivalent values"""
+
     annual_salary = input('Enter your annual salary: ')
     portion_saved = input('Enter the percent of your salary to save, as a decimal: ')
     total_cost = input('Enter the cost of your dream home: ')
@@ -25,11 +64,15 @@ def get_inputs():
 
 
 def display_output(value):
+    """Add value to output string and print to user"""
+
     output = 'Number of months: {}'.format(str(value))
     print(output)
 
 
 def calculate_monthly_current_savings_return(value, percentage):
+    """Calculate returns based on current value and going percentage"""
+
     return value * percentage / 12
 
 
